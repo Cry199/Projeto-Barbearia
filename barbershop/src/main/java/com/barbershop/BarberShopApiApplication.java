2d02be18e49b6af8753737e5f5d23a2f04782a46
Cauã-PC <caualeu2016@gmail.com>
2023-08-24 04:02:15 -0300

    esboço do site pronto e springboot funcionando v1

---
+++ barbershop/src/main/java/com/barbershop/BarberShopApiApplication.java
@@ -0,0 +1,14 @@
+package com.barbershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BarberShopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarberShopApiApplication.class, args);
+	}
+
+}
---
+++ barbershop/src/test/java/com/barbershop/BarberShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberShopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
