aee53f7de89458b15d85ba6a4b4ecc71ef213dcf
Cauã <caualeu2016@gmail.com>
2023-08-25 20:07:45 -0300

    v0.0.1 Reinicio

--- Api/src/main/resources/db/migration/V1__Create_Admin_Table.sql
+++
@@ -1,9 +0,0 @@
-
-
-CREATE TABLE admins (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(100) NOT NULL,
-    sobrenome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    senha VARCHAR(255) NOT NULL
-);
--- Api/src/main/resources/db/migration/V2__Create_Cliente_Table.sql
+++
@@ -1,9 +0,0 @@
-
-CREATE TABLE clientes (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(100) NOT NULL,
-    sobrenome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    senha VARCHAR(255) NOT NULL,
-    telefone VARCHAR(20) NOT NULL
-);
